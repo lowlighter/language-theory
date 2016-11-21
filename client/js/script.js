abef102be11ef8124f453766e86dfb7dca820a8e
Neabfi <fabien.delattre@outlook.com>
2016-11-21 10:17:22 +0100

    Auto close parenthesis

--- client/js/script.js
+++ client/js/script.js
@@ -3,12 +3,19 @@ var terminal = document.querySelector('#terminal');
 
 test = new WebSocketConnection("ws://localhost:8080/echo");
 
-input.addEventListener('keypress', function(e) {
-  if(e.key == "Enter") {
-    test.send(input.value);
-    addTerminal(input.value);
-    input.value = "";
-  }
+input.addEventListener('keyup', function(e) {
+	switch (e.key) {
+		case "Enter":
+			test.send(input.value);
+		    addTerminal(input.value);
+		    input.value = "";
+			break;
+		case "(":
+			var position = input.selectionStart;
+			input.value = [input.value.slice(0, position), ")", input.value.slice(position)].join('');
+		    input.setSelectionRange(position, position);
+			break;
+	}
 })
 
 function addTerminal(msg) {
