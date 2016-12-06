f20c68319371ba64fe64bed001ef0bcdf50929c6
Neabfi <fabien.delattre@outlook.com>
2016-12-06 21:54:35 +0100

    Avoid empty string

--- client/js/script.js
+++ client/js/script.js
@@ -3,31 +3,43 @@ var terminal = document.querySelector('#terminal');
 
 socketConnection = new WebSocketConnection("ws://localhost:8080/echo");
 
+// Gestion de l'historique avec les flèches du clavier
 var historyArr = [];
 historyPtr = 0;
 
-var a;
 
+// Grammaire côté client pour la coloration syntaxique
 var grammar = [{
 				rule: /[\+\-\*\/=\^]/gi,
 				color: "#FFCCFF"
 			},{
-				rule: /(cos|sin|plot|exp)/gi,
+				rule: /(cos|sin|plot|exp|acos|atan|asin|sinh|cosh|tanh|abs)/gi,
 				color: "#FFFFCC"
 			},{
 				rule: /[\(\)]/gi,
 				color: "#CCFFFF"
 			}];
 
+
+
+
 input.addEventListener('keyup', function(e) {
+
+	// Coloration syntaxique de l'entrée de l'utilisateur
 	highlighter('#input', grammar);
+
 	switch (e.key) {
+		// Appuie sur la touche entrer
 		case "Enter":
 			if(strip(input.innerHTML) == "clear") {
 				clearTerminal();
 				input.innerHTML = "";
 				break;
+			} else if(input.innerHTML == "<br>")
+			{
+				break;	
 			}
+			console.log(input.innerHTML)
 			socketConnection.send(strip(input.innerHTML));
 		    appendTextInTerminal(highlighterLight('>>> ' + input.innerHTML, grammar));
 		    terminal.scrollTop = terminal.scrollHeight;
@@ -35,12 +47,16 @@ input.addEventListener('keyup', function(e) {
 		    historyPtr = historyArr.length;
 		    input.innerHTML = "";
 			break;
+
+		// Appuie sur la flèche du haut
 		case "ArrowUp":
 			if(historyPtr > 0) {
 				historyPtr -- ;
 				input.innerHTML = historyArr[historyPtr];
 			}
 			break;
+
+		// Appuie sur la flèche du bas
 		case "ArrowDown":
 			if(historyPtr < historyArr.length-1) {
 				historyPtr ++ ;
